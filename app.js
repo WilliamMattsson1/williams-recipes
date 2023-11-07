589e16970bffcc9c2764a7ab948e98f45a4b8250
William Mattsson <wmattsson@hotmail.com>
2023-11-07 09:47:27 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,349 @@
+const modeBtn = document.querySelector('.mode-btn > i')
+const favContainer = document.querySelector('.fav-meals-container')
+const favArrow = document.querySelector('.fav-toggle')
+const favMealsContainer = document.querySelector('.fav-meals')
+
+const searchInput = document.querySelector('.search-input')
+const searchBtn = document.querySelector('.search-btn')
+const liSuggestions = document.querySelectorAll('.suggestions > li')
+const randomBtn = document.querySelector('.random-btn')
+
+const mealsH3 = document.querySelector('.meals-h3')
+const mealsContainer = document.querySelector('.meals')
+
+const popupContainer = document.querySelector('.pop-up-container')
+const popupH2 = document.querySelector('.popup-h2')
+const closePopupBtn = document.querySelector('.pop-up > i')
+const popup = document.querySelector('.pop-up-content')
+
+const footer = document.querySelector('footer')
+
+// Open/close hamburger menu
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links')
+    const icon = document.querySelector('.hamburger-icon')
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+}
+
+// Change the icon when mode switching and toggle class
+modeBtn.addEventListener('click', () => {
+    if (modeBtn.classList.contains('fa-toggle-off')) {
+        modeBtn.setAttribute('class', 'fa-solid fa-toggle-on')
+    } else {
+        modeBtn.setAttribute('class', 'fa-solid fa-toggle-off')
+    }
+    document.documentElement.classList.toggle('dark-mode')
+})
+
+// When search icon is pressed.
+searchBtn.addEventListener('click', showResults)
+// When Enter is pressed
+searchInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        showResults()
+    }
+})
+
+/* Showing results from search value */
+async function showResults() {
+    mealsContainer.innerHTML = '' // Empty from previous.
+    const searchValue = searchInput.value
+    const meals = await fetchMealBySearch(searchValue) // Fetch meals from user input.
+
+    // If there is search results display all results
+    if (meals) {
+        meals.forEach((meal) => {
+            displayMeal(meal)
+        })
+
+        // Change the text from random meal to this
+        mealsH3.innerText = `Search results for: ${searchInput.value}`
+    }
+
+    // If there isnt any search results.
+    else {
+        mealsH3.innerText = 'No meals found...'
+        mealsContainer.innerHTML = ''
+    }
+    // Clear the input field
+    searchInput.value = ''
+}
+
+// Fetch meals by the user input and returns
+async function fetchMealBySearch(input) {
+    try {
+        const response = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+        )
+        const data = await response.json()
+        const meals = data.meals
+        return meals
+    } catch (error) {
+        console.error('Something went wrong: ', error)
+    }
+}
+
+// Add meal to mealsContainer, logic behind favorite button and show popup if its pressed
+function displayMeal(meal) {
+    const mealCard = document.createElement('div')
+    mealCard.classList.add('meal-card')
+    mealCard.innerHTML = `
+            <div class="meal-card-img-container">
+                <img src="${meal.strMealThumb}">
+            </div>
+            <div class="meal-name">
+                <i class="fa-solid fa-utensils"></i>
+                <p>${meal.strMeal}</p>
+                <i class="fa-regular fa-heart"></i>
+            </div>
+    `
+
+    /* shows popup when utensils btn  */
+    const utensilsBtn = mealCard.querySelector('.fa-utensils')
+    utensilsBtn.addEventListener('click', () => {
+        showMealPopup(meal)
+    })
+
+    const favBtn = mealCard.querySelector('.fa-heart')
+    favBtn.addEventListener('click', () => {
+        if (favBtn.classList.contains('fa-regular')) {
+            favBtn.setAttribute('class', 'fa-solid fa-heart')
+            addMealStorage(meal.idMeal) // Save id to Local storage
+        } else {
+            favBtn.setAttribute('class', 'fa-regular fa-heart')
+            removeMealStorage(meal.idMeal)
+        }
+        updateFavMeals()
+    })
+
+    // show popup when meal img is pressed
+    mealCard
+        .querySelector('.meal-card-img-container')
+        .addEventListener('click', () => {
+            showMealPopup(meal)
+        })
+
+    mealsContainer.append(mealCard)
+}
+
+// Listen to the suggestions and send the category to the function
+liSuggestions.forEach((li) => {
+    li.addEventListener('click', () => {
+        const category = li.innerText
+        fetchMealsByCategory(category)
+    })
+})
+
+// Display the results depending on the category
+async function fetchMealsByCategory(category) {
+    try {
+        const response = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+        )
+        const data = await response.json()
+        const meals = data.meals
+        mealsContainer.innerHTML = ''
+
+        console.log(meals)
+
+        if (meals) {
+            meals.forEach((meal) => {
+                displayMeal(meal)
+            })
+
+            // Change the text from random meal to this
+            mealsH3.innerText = `Search results for: ${category}`
+        }
+
+        // If there isnt any search results.
+        else {
+            mealsH3.innerText = 'No meals found...'
+            mealsContainer.innerHTML = ''
+        }
+    } catch {
+        console.error('Something went wrong: ', error)
+    }
+}
+
+// Display one random meal when btn is clicked
+randomBtn.addEventListener('click', () => {
+    mealsContainer.innerHTML = ''
+    mealsH3.innerText = `Random generated meal`
+    displayRandomMeal()
+})
+
+// Generate a random meal and calls displayMeal(random)
+async function displayRandomMeal() {
+    try {
+        const response = await fetch(
+            'https://www.themealdb.com/api/json/v1/1/random.php'
+        )
+        const data = await response.json()
+        const randomMeal = data.meals[0]
+        console.log(randomMeal)
+        displayMeal(randomMeal)
+    } catch (error) {
+        console.error('Something went worng: ', error)
+    }
+}
+
+async function updateFavMeals() {
+    favMealsContainer.innerHTML = ''
+    const mealsIds = getMealStorage() // gets the meals from ls
+    for (let i = 0; i < mealsIds.length; i++) {
+        const mealID = mealsIds[i]
+        const meal = await fetchMealById(mealID) // get every meals id
+        addMealToFav(meal) // create the element with next function
+    }
+}
+
+// fetch the meal by ID and returns it
+async function fetchMealById(id) {
+    try {
+        const response = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+        )
+        const data = await response.json()
+        const meal = data.meals[0]
+        return meal
+    } catch (error) {
+        console.log('Something went wrong: ', error)
+    }
+}
+
+//fetch the favorites
+function addMealToFav(meal) {
+    const favMeal = document.createElement('div')
+    favMeal.innerHTML = `
+            <div class="fav-meal">
+                <div class="fav-meal-content">
+                    <div class="fav-meal-img-container">
+                        <img src="${meal.strMealThumb}" alt="fav-meal image">
+                    </div>
+                    <div class="fav-meal-text">
+                        <p>${meal.strMeal}</p>
+                    </div>
+                </div>
+                <i class="fa-solid fa-x"></i>
+            </div>
+    `
+    // The X take it away from fav list and LS
+    const x = favMeal.querySelector('.fa-x')
+    x.addEventListener('click', () => {
+        removeMealStorage(meal.idMeal)
+
+        // Update the favorite button too
+        const heartBtns = document.querySelectorAll('.fa-heart')
+        heartBtns.forEach((heartBtn) => {
+            heartBtn.setAttribute('class', 'fa-regular fa-heart')
+        })
+
+        // Update the fav list
+        updateFavMeals()
+    })
+
+    // Shows popup when the img is clicked.
+    favMeal.querySelector('.fav-meal-content').addEventListener('click', () => {
+        showMealPopup(meal)
+    })
+
+    // Places it in the container.
+    favMealsContainer.append(favMeal)
+}
+
+// Get the local storage if there is something there.
+function getMealStorage() {
+    const mealIds = JSON.parse(localStorage.getItem('mealIds'))
+
+    return mealIds === null ? [] : mealIds
+}
+
+// Add meal to Local storage array
+function addMealStorage(mealID) {
+    const mealIds = getMealStorage()
+    localStorage.setItem('mealIds', JSON.stringify([...mealIds, mealID]))
+}
+
+// Remove meal from local storage array
+function removeMealStorage(mealID) {
+    const mealIds = getMealStorage()
+    localStorage.setItem(
+        'mealIds', // Creates new array with id's that dont match mealID
+        JSON.stringify(mealIds.filter((id) => id !== mealID))
+    )
+}
+
+// Popup function
+function showMealPopup(meal) {
+    popup.innerHTML = ''
+    //  Hide footer when popup comes
+    footer.style.display = 'none'
+
+    // Creates the element and adds style
+    const newPopup = document.createElement('div')
+    newPopup.classList.add('pop-up-content')
+
+    // If there is ingridients --> Push in to array. ELSE break the loop.
+    const ingredients = []
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            )
+        } else {
+            break
+        }
+    }
+
+    popupH2.innerText = `${meal.strMeal}`
+
+    newPopup.innerHTML = `
+    <div class="left">
+        <div class="meal-card">
+            <div class="meal-card-img-container">
+                <img src="${meal.strMealThumb}" alt="recipe foto">
+            </div>
+            <div class="meal-name">
+                <p>${meal.strMeal}</p>
+                <i class="fa-regular fa-heart"></i>
+            </div>
+        </div>
+        <div class="ingredients-div">
+            <h2>Ingredients / Measures</h2>
+            <ul>
+                ${ingredients.map((i) => `<li>${i}</li>`).join('')}
+            </ul>
+        </div>
+    </div>
+    <div class="right">
+        <div>
+            <h2>Intructions</h2>
+            <p class="meal-info">${meal.strInstructions}</p>
+        </div>
+
+    </div>
+`
+
+    popup.append(newPopup)
+    popupContainer.style.display = 'flex' // Make it visible
+}
+
+// Close the popup when X is clicked
+closePopupBtn.addEventListener('click', () => {
+    popupContainer.style.display = 'none'
+    footer.style.display = 'block'
+})
+
+// Toggle the fav meals
+favArrow.addEventListener('click', () => {
+    if (favArrow.classList.contains('fa-arrow-down')) {
+        favArrow.setAttribute('class', 'fa-solid fa-arrow-up fav-toggle')
+        favContainer.style.height = 'auto'
+    } else {
+        favArrow.setAttribute('class', 'fa-solid fa-arrow-down fav-toggle')
+        favContainer.style.height = '2.4rem'
+    }
+})
+
+updateFavMeals()
---
+++ chart.js
@@ -0,0 +1,73 @@
+// * To display how many meals we have for every category
+
+// ! Make sure this is linked with <script> in the about.html
+let categoryNames = []
+let mealsQuantity = []
+const ctx = document.getElementById('my-chart')
+
+// Get all categories and create the chart
+async function fetchCategories() {
+    try {
+        const response = await fetch(
+            'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
+        )
+        const data = await response.json()
+        const categories = data.meals
+        categoryNames = categories.map((category) => category.strCategory)
+
+        for (let i = 0; i < categoryNames.length; i++) {
+            await getQuantity(categoryNames[i])
+        }
+
+        console.log(categoryNames)
+        console.log(mealsQuantity)
+
+        createChart() // Create the chart after fetching data
+    } catch (error) {
+        console.log('Something went wrong:', error)
+    }
+}
+
+// get the meals from the (categoryName)
+async function getQuantity(categoryName) {
+    try {
+        const response = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+        )
+        const data = await response.json()
+        const category = data.meals
+        let quantity = category.length
+        mealsQuantity.push(quantity)
+    } catch (error) {
+        console.log('Something went wrong:', error)
+    }
+}
+
+// Function to create the chart
+function createChart() {
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: categoryNames,
+            datasets: [
+                {
+                    label: 'Different Categories',
+                    data: mealsQuantity,
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                    borderColor: ['rgba(255, 99, 132, 1)'],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+}
+
+// Start the process
+fetchCategories()
